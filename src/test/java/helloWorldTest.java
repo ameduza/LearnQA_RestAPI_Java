c5b27055b4aa24cc70b66d742f49ab882a974276
Alexander Maximov <maximova@odysseyvc.com>
2022-09-07 22:23:30 +0300

    First GET request

--- src/test/java/helloWorldTest.java
+++ src/test/java/helloWorldTest.java
@@ -1,9 +1,22 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.io.Console;
 
 public class helloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("HelloWorld");
+    public void testHelloFromAlex() {
+        System.out.println("Hello from Alex");
     }
+
+    @Test
+    public void testGetRequest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
 }
