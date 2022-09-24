3cbc018f3259527ab3c51a38ef07d3108b8cd882
Alexander Maximov <maximova@odysseyvc.com>
2022-09-24 18:00:14 +0300

    Lesson2. Exercise 7

--- src/test/java/Lesson5.java
+++ src/test/java/Lesson5.java
@@ -17,7 +17,7 @@ public class Lesson5 {
     }
 
     @Test
-    public void testExercise6(){
+    public void testExercise6() {
 // Необходимо написать тест, который создает GET-запрос на адрес: https://playground.learnqa.ru/api/long_redirect
 // С этого адреса должен происходит редирект на другой адрес. Наша задача — распечатать адрес, на который редиректит указанные URL.
 // Ответом должна быть ссылка на тест в вашем репозитории.
@@ -31,5 +31,31 @@ public class Lesson5 {
 
         String redirectUrl = response.getHeader("Location");
         System.out.println(redirectUrl);
+    }
+
+    @Test
+    public void testExercise7() {
+//  Необходимо написать тест, который создает GET-запрос на адрес из предыдущего задания: https://playground.learnqa.ru/api/long_redirect
+//  На самом деле этот URL ведет на другой, который мы должны были узнать на предыдущем занятии. Но этот другой URL тоже куда-то редиректит.
+//  И так далее. Мы не знаем заранее количество всех редиректов и итоговый адрес.
+//
+//  Наша задача — написать цикл, которая будет создавать запросы в цикле, каждый раз читая URL для редиректа из нужного заголовка.
+//  И так, пока мы не дойдем до ответа с кодом 200.
+
+        Response response;
+        int responseCode = 0;
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+
+        while (responseCode != 200) {
+            response = RestAssured
+                    .given()
+                    .redirects().follow(false)
+                    .get(url)
+                    .andReturn();
+            responseCode = response.getStatusCode();
+            url = response.getHeader("Location");
+
+            System.out.println(responseCode + " " + url);
+        }
     }
 }
