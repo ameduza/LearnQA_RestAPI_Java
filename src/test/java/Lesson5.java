134916137e0c2e04232ac268f9340474c03d4789
Alexander Maximov <maximova@odysseyvc.com>
2022-09-26 18:26:36 +0300

    Lesson2. Exercise 8

--- src/test/java/Lesson5.java
+++ src/test/java/Lesson5.java
@@ -2,6 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 public class Lesson5 {
 
     @Test
@@ -58,4 +62,64 @@ public class Lesson5 {
             System.out.println(responseCode + " " + url);
         }
     }
+
+    @Test
+    public void testExercise8() throws InterruptedException {
+        boolean isNotFinished = true;
+        Map<String, String> params = new HashMap<>();
+
+        LongTimeJobApiResponse response = getJobRequest(params, true);
+        params.put("token", response.token);
+        System.out.println("Job created, let's wait for at least " + response.seconds + " seconds");
+        TimeUnit.SECONDS.sleep(response.seconds);
+
+        while (isNotFinished) {
+            response = getJobRequest(params, false);
+            String status = response.status;
+            switch (status) {
+                case "Job is NOT ready":
+                    System.out.println("...hm, still not ready. Wait for another 500 msec");
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    break;
+                case "Job is ready":
+                    isNotFinished = false;
+                    System.out.println("Job complete");
+                    System.out.println("Result: " + response.result);
+                    break;
+                default:
+                    System.out.println("Invalid response!");
+                    break;
+            }
+        }
+    }
+
+    private LongTimeJobApiResponse getJobRequest(Map<String, String> params, boolean jobCreateRequest) {
+        Response r = RestAssured
+                .given()
+                .with().params(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        LongTimeJobApiResponse obj = new LongTimeJobApiResponse();
+        if (jobCreateRequest) {
+            obj.token = r.getBody().path("token");
+            obj.seconds = (int) r.getBody().path("seconds");
+
+        } else {
+            obj.error = r.getBody().path("error");
+            obj.status = r.getBody().path("status");
+            obj.result = r.getBody().path("result");
+        }
+        return obj;
+    }
+
+}
+
+class LongTimeJobApiResponse {
+    String token;
+    int seconds;
+    String error;
+    String status;
+    String result;
+
 }
